29f70d46ee492b1012523917cd992e3bfef8a269
thomas <hedywqy@gmail.com>
2022-07-11 00:05:28 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hedywqy/vuepress.git main:gh-pages
+git push -f git@github.com:hedywqy/vuepress.git master:gh-pages
 
 cd -
